4d0da400e1c37ecfc4531fa9abd1f69e2660acd3
rickyrod4 <dj3d@Ricardos-MacBook-Air.local>
2024-07-16 13:52:52 -0500

    Empty

--- Main.java
+++ Main.java
@@ -1,6 +1,30 @@
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 public class Main{
     public static void main(String[] args) {
+
+        static void myMethod(){
+            System.out.println("I have been executed");
+        }
         //Array - Used to store multiple values in a single variable
-        
+        int nums [] = new int [] {10,62,3,14,55,6};
+        System.out.println(Arrays.toString(nums));
+        for(int i =0; i<nums.length;i++){
+            for(int j = i+1;j<nums.length;j++){
+                if(nums[i] < nums[j]){
+                    int replace = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = replace;
+                }
+            }
+            myMethod();
+        }
+
     }
+
+
+
+
+
 }
