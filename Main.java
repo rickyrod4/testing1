7c1e362edb5df8aae21160c49c37991edb42fee0
rickyrod4 <dj3d@Ricardos-MacBook-Air.local>
2024-07-16 12:46:22 -0500

    Starting over Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
+public class Main{
+    public static void main(String[] args) {
+        //Array - Used to store multiple values in a single variable
+        String [] cars = new String[3];
+        cars[0] = "Camero";
+        cars[1] = "Corvette";
+        cars[2] = "Tesla";
 
-public class Main {
-    public static void main(String []args){
-        String [] cars = {"Ford", "Kia", "Toyota"};
-        cars[0]= "Mustang";
-        System.out.println(cars[0]);
-
+        for(int i =0; i<cars.length;i++){
+            System.out.println(cars[i]);
+        }
     }
 }
-
--- arr.py
+++
@@ -1,8 +0,0 @@
-print("Testing Arrays in Python")
-
-cars = ["Ford", "Lexus", "Toyota", "Kia", "Chevrolet"]
-
-
-
-for x in cars:
-    print(x)
