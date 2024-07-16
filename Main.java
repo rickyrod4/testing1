19b34607446f6a3f9c85fd380dcccc016200d23f
rickyrod4 <dj3d@Ricardos-MacBook-Air.local>
2024-07-16 12:47:28 -0500

    Modified Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,6 @@
 public class Main{
     public static void main(String[] args) {
         //Array - Used to store multiple values in a single variable
-        String [] cars = new String[3];
-        cars[0] = "Camero";
-        cars[1] = "Corvette";
-        cars[2] = "Tesla";
-
-        for(int i =0; i<cars.length;i++){
-            System.out.println(cars[i]);
-        }
+        
     }
 }
